1ef57cab66abf716ea1d40d9303de64e10fd660e
Mitroshin Dmitry <88803118+ZzVaMPIRzZ@users.noreply.github.com>
2023-09-23 09:43:05 +0000

    del comments

--- lab1/rand.sh
+++ lab1/rand.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 SIZE=150;
-# while (( $SIZE > 0 )); do
 RANDOM_INT=$(od -An -t d -N"$(($SIZE*4))" /dev/random);
 echo -n $RANDOM_INT > numbers.txt;
-# echo -n " " >> numbers.txt;
-# let "SIZE-=1";
-# done
