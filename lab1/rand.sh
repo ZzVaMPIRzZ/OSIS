ba89332bee2e50476452e4957be60766df7b7d58
Mitroshin Dmitry <88803118+ZzVaMPIRzZ@users.noreply.github.com>
2023-09-23 09:21:37 +0000

    123

--- lab1/rand.sh
+++ lab1/rand.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 SIZE=150;
-while (( $SIZE > 0 )); do
-RANDOM_INT=$(od -An -t d -N 1 /dev/random);
-echo -n $RANDOM_INT >> numbers.txt;
-echo -n " " >> numbers.txt;
-let "SIZE-=1";
-done
+# while (( $SIZE > 0 )); do
+RANDOM_INT=$(od -An -t d -N"$(($SIZE*4))" /dev/random);
+echo -n $RANDOM_INT > numbers.txt;
+# echo -n " " >> numbers.txt;
+# let "SIZE-=1";
+# done
