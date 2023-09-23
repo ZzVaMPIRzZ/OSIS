5d475691b8c97b5429151483518ae569c8b0acde
Mitroshin Dmitry <88803118+ZzVaMPIRzZ@users.noreply.github.com>
2023-09-23 09:43:05 +0000

    del comments

--- lab3/src/go.c
+++ lab3/src/go.c
@@ -5,6 +5,5 @@ int main(int argc, char **argv){
 	if ( pid == 0 ) {
 		execv( "./sequential_min_max", argv);
 	}
-	/* Put the parent to sleep for 2 seconds--let the child finished executing */
     return 0;
 }
